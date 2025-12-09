9cfe117d5035c70bc3c176665003fe37ceda7509
LunKEA2024 <maaq0001@stud.kea.dk>
2025-12-09 14:16:42 +0100

    GSAP WORKS BABY

--- src/pages/script.js
+++ src/pages/script.js
@@ -1,49 +1,32 @@
-import { start } from "alpinejs";
-import { triggerEvent } from "astro/virtual-modules/transitions-events.js";
-import { easeIn, maxGeneratorDuration } from "motion";
+// import { start } from "alpinejs";
+// import { triggerEvent } from "astro/virtual-modules/transitions-events.js";
+// import { easeIn, maxGeneratorDuration } from "motion";
 
+// import { gsap } from "gsap";
+
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { gsap } from "gsap";
 
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.to(".a", {
+  x: 400,
+  rotation: 360,
+  duration: 6,
+});
 
-gsap.registerPlugin(ScrollTrigger);
+// gsap.to(".c", {
+//   ScrollTrigger: {
+//     trigger: ".c",
+//     start: "top center",
+//     markers: true,
+//     toggleActions: "play pause resume reset",
+//   },
+//   x: 400,
+//   rotation: 360,
+//   duration: 6,
+// });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const texts = [
-    { id: "text-1", endTranslateX: -2000, rotate: 45 },
-    { id: "text-2", endTranslateX: -2000, rotate: -35 },
-    { id: "text-3", endTranslateX: -2000, rotate: 45 },
-  ];
+const box = document.querySelector(".a");
 
-  ScrollTrigger.create({
-    trigger: ".wrapper",
-    start: "top top",
-    end: "+=900vh",
-    scrub: 1,
-    pin: true,
-    onupdate: (self) => {
-      gsap.to(".wrapper", {
-        x: `${350 * self.progress}vw`,
-        durtion: 0.5,
-        easeIn: "power3.inOut",
-      });
-    },
-  });
-
-  texts.forEach((text) => {
-    ScrollTrigger.create({
-      trigger: "text.id",
-      start: "top top",
-      end: "+=1200vh",
-      scrub: 1,
-      onUpdate: (self) => {
-        gsap.to(text.id, {
-          x: `${text.endTranslateX * self.progress}px`,
-          rotation: `${text.rotate * self.progress * 2}`,
-          duration: 0.5,
-          easeIn: "power3.out",
-        });
-      },
-    });
-  });
+box.addEventListener("click", (e) => {
+  console.log(e);
 });
