2ba9b94d2702c81c42ff8a05eaa211942d338281
LunKEA2024 <maaq0001@stud.kea.dk>
2025-12-09 17:08:58 +0100

    GSAP Scrolltrigger works

--- src/pages/script.js
+++ src/pages/script.js
@@ -1,32 +1,13 @@
-// import { start } from "alpinejs";
-// import { triggerEvent } from "astro/virtual-modules/transitions-events.js";
-// import { easeIn, maxGeneratorDuration } from "motion";
-
-// import { gsap } from "gsap";
-
-// import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".a", {
-  x: 400,
+gsap.to(".c", {
+  scrollTrigger: {
+    trigger: ".c",
+    toggleActions: "restart none none none",
+  },
+  x: 1400,
   rotation: 360,
   duration: 6,
-});
-
-// gsap.to(".c", {
-//   ScrollTrigger: {
-//     trigger: ".c",
-//     start: "top center",
-//     markers: true,
-//     toggleActions: "play pause resume reset",
-//   },
-//   x: 400,
-//   rotation: 360,
-//   duration: 6,
-// });
-
-const box = document.querySelector(".a");
-
-box.addEventListener("click", (e) => {
-  console.log(e);
 });
