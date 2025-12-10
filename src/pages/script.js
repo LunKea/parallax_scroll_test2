6bcac2dec24e8fff4fa988002b102cf07aeb7b40
LunKEA2024 <maaq0001@stud.kea.dk>
2025-12-10 18:51:37 +0100

    modified:   src/pages/index.astro 	modified:   src/pages/script.js

--- src/pages/script.js
+++ src/pages/script.js
@@ -2,12 +2,45 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
+// gsap.to(".c", {
+//   scrollTrigger: {
+//     trigger: ".c",
+//     toggleActions: "restart pause reverse pause",
+//   },
+//   x: 1400,
+//   rotation: 360,
+//   duration: 6,
+// });
+
+gsap.to(".a", {
+  scrollTrigger: {
+    trigger: ".a",
+    toggleActions: "restart none reverse none",
+  },
+  y: -50,
+  autoAlpha: 1,
+  duration: 3,
+});
+
+gsap.to(".b", {
+  scrollTrigger: {
+    trigger: ".b",
+    toggleActions: "restart none reverse none",
+  },
+  y: -50,
+  autoAlpha: 1,
+  duration: 3,
+  delay: 0.3,
+  });
+
 gsap.to(".c", {
   scrollTrigger: {
     trigger: ".c",
     toggleActions: "restart none reverse none",
   },
-  x: 1400,
-  rotation: 360,
-  duration: 6,
-});
+  y: -50,
+  autoAlpha: 1,
+  duration: 3,
+  delay: 0.6,
+
+  });
